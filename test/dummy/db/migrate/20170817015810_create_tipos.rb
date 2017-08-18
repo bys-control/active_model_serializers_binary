ef4f8b6cedd376506f195c328c076973d121d4b0
Guillermo Bisheimer <gbisheimer@bys-control.com.ar>
2017-08-17 23:53:16 -0300

    Update test/dummy app

--- test/dummy/db/migrate/20170817015810_create_tipos.rb
+++
@@ -1,9 +0,0 @@
-class CreateTipos < ActiveRecord::Migration[5.0]
-  def change
-    create_table :tipos do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- test/dummy/lib/test_parser.rb
+++ test/dummy/lib/test_parser.rb
@@ -7,10 +7,28 @@ class String
   end
 end
 
-orig = Producto.new
-orig.silo = 0xFF7F
-orig.nombre = "AAAAAAAAAAAAAAAAAAAA"
-orig.tipo = Tipo.new({name:"BBBBBBBBBBBBBBBBBBBB", producto_id: 0xFF7F})
+orig = Product.new
+
+orig.int8 = 1;
+orig.int16 = 1;
+orig.int16le = 1;
+orig.int16be = 1;
+orig.int32 = 1;
+orig.int32le = 1;
+orig.int32be = 1;
+orig.uint8 = 1;
+orig.uint16 = 1;
+orig.uint16le = 1;
+orig.uint16be = 1;
+orig.uint32 = 1;
+orig.uint32le = 1;
+orig.uint32be = 1;
+orig.bitfield = 1;
+orig.float32 = 1;
+orig.float64 = 1;
+orig.char = "A";
+orig.bool = 1;
+orig.type = Type.new({product_id: 1, name: "ABCDEFGHIJKLMNOPQRST"})
 
 puts 'Datos originales...'
 puts orig.inspect.green
@@ -21,7 +39,7 @@ serial = orig.to_bytes
 puts serial.inspect.yellow
 
 puts 'deserializando...'
-deser = Producto.new.from_bytes serial
+deser = Product.new.from_bytes serial
 puts deser.inspect.green
 
 must_be_equal = (serial <=> deser.to_bytes) === 0
